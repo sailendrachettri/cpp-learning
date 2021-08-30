346b7e84b995e02f5b53ab1626f39155f9be11f2
sailendrachettri <sailendra9083@gmail.com>
2021-08-30 12:16:01 +0530

    polymorphism intro

---
+++ file9/tut54.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // poly - many
+    // morphism - forms
+
+    /*
+        Polymorphism
+            - one name and multiple forms
+            - eg. function overloading
+    */
+    cout<< "\nPolymorphism in C++\n"<<endl;    
+
+    return 0;
+}
